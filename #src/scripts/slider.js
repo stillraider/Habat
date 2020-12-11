25bb79ab8fb1c06783608611e3fcacd74f3fcc81
stillraider <61648212+stillraider@users.noreply.github.com>
2020-12-11 12:09:26 +0500

    init

---
+++ #src/scripts/slider.js
@@ -0,0 +1,11 @@
+// new Swiper('.slider',{
+//     navigation: {
+//         nextEl: '.swiper-button-next',
+//         prevEl: '.swiper-button-prev'
+//     },
+//     pagination: {
+//         el: '.swiper-pagination',
+//         clickable: true
+//     },
+//     loop: true,
+// })
